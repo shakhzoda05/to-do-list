8e46767886e1bd63098f5e5ed7fa81f4723fb004
shakhzoda05 <shakhzoda05@gmail.com>
2024-07-10 10:08:59 +0500

    change

--- js/main.js
+++ js/main.js
@@ -21,8 +21,8 @@ function renderTodos(arr,list){
         let elItem=document.createElement("li")
 
         elItem.className="flex items-center justify-between"
-        elItem.innerHTML =
-            <div>
+        elItem.innerHTML =`
+                <div>
                     <span class="font-bold text-[20px] text-slate-500">${item.id}</span>
                     <span class="font-bold text-[22px]">${item.value}</span>
                 </div>
@@ -31,11 +31,8 @@ function renderTodos(arr,list){
                     <button class="bg-red-500 hover:opasity-50 duration-300 text-white font-semibold p-3 rounded-lg">Delete</button>
                 </div>
 
-// ---------------------------XATOYIMNI TOPOLMAYAPMAN DARSDA USTOZDAN SORAB TO'G'IRLAB QO'YAMAN------------------------------------------------
-        
-       
-        
-        
+        `
+           
         
         list.appendChild(elItem);
     })
